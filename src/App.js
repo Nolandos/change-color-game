6288fd355312dd7192227147aafd53f85b7b8309
Łukasz Nolka <nolkalukasz@gmail.com>
2019-11-27 06:09:30 +0100

    Init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
 
-function App() {
+/* Import utils */
+import mathRandom from './utils/mathRandom';
+
+const App = () => {
+  const [state, setState] = useState({
+    initialBoard: new Array(20)
+  });
+
+  useEffect(() => {
+    //console.log(state.initialBoard)
+    state.initialBoard.forEach(field => {
+      console.log(field)
+    })
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
 
